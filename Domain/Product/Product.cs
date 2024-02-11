f94a15757b5a9e9c66f59b3edd149538b272ce33
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-12 00:55:04 +0530

    I can Little bit understand -> Clean Architecture

--- Domain/Product/Product.cs
+++ Domain/Product/Product.cs
@@ -8,6 +8,14 @@ namespace Domain.Product
 {
     public class Product
     {
+        public Product(ProductId id, string name, Money price, Sku sku)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Sku = sku;
+        }
+
         public ProductId Id { get; private set; }
         public string Name { get; private set; } = string.Empty;
         public Money Price { get; private set; }
---
+++ WebApi/EndPoints/Products.cs
@@ -0,0 +1,18 @@
+﻿using Application.Product.Create;
+using Carter;
+
+namespace WebApi.EndPoints
+{
+    public class Products : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("product", async (CreateProductCommand command, ISender sender) =>
+            {
+                await sender.Send(command);
+
+                return Results.Ok();
+            });
+        }
+    }
+}
