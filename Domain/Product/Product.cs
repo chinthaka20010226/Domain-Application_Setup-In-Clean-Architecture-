d2e1d8135584a8b3e4337ace1cd87fa9a48666d8
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-11 14:49:21 +0530

    Product Entity -> its Attributes -> data_type of Attributes

---
+++ Domain/Product/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Product
+{
+    public class Product
+    {
+        public ProductId Id { get; private set; }
+        public string Name { get; private set; } = string.Empty;
+        public Money Price { get; private set; }
+        public Sku Sku { get; private set; }
+    }
+}
---
+++ Domain/Product/Sku.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Product;
+
+public record Sku
+{
+    private const int DefaultLength = 8;
+
+    private Sku(string value) => Value = value;
+
+    // init -> The init accessor indicates that this property can only be set during object initialization,
+    // making the Sku class immutable after creation.
+    public string Value { get; init; }
+
+    // Sku? -> nullable
+    public static Sku? Create(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return null;
+        }
+        if(value.Length != DefaultLength)
+        {
+            return null;
+        }
+        return new Sku(value);
+    }
+}
